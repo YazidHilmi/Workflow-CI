bf510181521fc5bf72fe506764cf7425e61469c6
Yazid Hilmi <yazidhilmi10@gmail.com>
2025-12-22 23:36:36 +0700

    Update file modelling py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -6,48 +7,46 @@ from sklearn.preprocessing import StandardScaler
 import mlflow
 import mlflow.sklearn
 
-# Tracking lokal
-mlflow.set_tracking_uri(uri="http://127.0.0.1:5000/")
 
-# Autolog
+# Argument parsing
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_estimators", type=int, default=100)
+parser.add_argument("--max_depth", type=int, default=None)
+parser.add_argument("--random_state", type=int, default=42)
+args = parser.parse_args()
+
+# Tracking 
+mlflow.set_tracking_uri("file:./mlruns")
+
 mlflow.sklearn.autolog()
 
 # Load Dataset
-df = pd.read_csv('preprocessing/Titanic_Dataset_Analysis_preprocessing.csv')
+df = pd.read_csv("Titanic_Dataset_Analysis_preprocessing.csv")
 
-X = df.drop('Survived', axis=1)
-y = df['Survived']
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42
+    X, y, test_size=0.2, random_state=args.random_state
 )
 
-# Scaling
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Experiment
 mlflow.set_experiment("Titanic Random Forest")
 
 with mlflow.start_run():
-
     model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
+        n_estimators=args.n_estimators,
+        max_depth=args.max_depth,
+        random_state=args.random_state
     )
 
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-
-    # Metric tambahan (opsional)
+    accuracy = model.score(X_test, y_test)
     mlflow.log_metric("accuracy_manual", accuracy)
 
     print(f"Accuracy: {accuracy:.2f}")
-
-
